359536669d2dfe0e2f4ced5acfea09c2f3679e29
pearpages <pere@soms.cat>
2015-11-13 15:44:51 +0100

    ui.mask

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function (){
 	"use strict";
 	angular
-	.module('app',['common.services','ui.router','productResourceMock'])
+	.module('app',['common.services','ui.router','productResourceMock','ui.mask'])
 	.config(['$stateProvider','$urlRouterProvider',function ($stateProvider,$urlRouterProvider) {
 		$stateProvider
 		.state('home',{
