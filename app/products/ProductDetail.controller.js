0a2203adf55f019db8728a9a9771e73138b52482
pearpages <pere@soms.cat>
2015-11-15 18:06:20 +0100

    custom service

--- app/products/ProductDetail.controller.js
+++ app/products/ProductDetail.controller.js
@@ -2,19 +2,24 @@
 	"use strict";
 
 	angular
-		.module("app")
-		.controller("ProductDetailController",['product',ProductDetailController]);
+	.module("app")
+	.controller("ProductDetailController",['product','productsService',ProductDetailController]);
 
-	function ProductDetailController(product) {
+	function ProductDetailController(product,productsService) {
 
 		var vm = this;
 
 		vm.product = product;
-		
 		vm.title = "Product Detail: " + vm.product.productName;
+		vm.marginPercent = productsService.calculateMarginPercent(vm.product.price,vm.product.cost);
 
-		if(vm.product.tags){
-			vm.product.tagList = vm.product.tags.toString();
+		activate();
+
+		function activate() {
+			if(vm.product.tags){
+				vm.product.tagList = vm.product.tags.toString();
+			}	
 		}
+		
 	}	
 })();
