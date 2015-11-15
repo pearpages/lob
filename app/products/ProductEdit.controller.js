2d606653313b720a66600f9c417da77dd07170cb
pearpages <pere@soms.cat>
2015-11-15 13:16:43 +0100

    search form

--- app/products/ProductEdit.controller.js
+++ app/products/ProductEdit.controller.js
@@ -1,25 +1,29 @@
 (function () {
 	angular
-		.module('app')
-		.controller('ProductEditController',['product','$state',ProductEditController]);
+	.module('app')
+	.controller('ProductEditController',['product','$state',ProductEditController]);
 
 	function ProductEditController(product, $state) {
 		//product gets populated in the resolve in the app.js
 		var vm = this;
 
 		vm.product = product;
-
 		vm.open = open;
-
 		vm.submit = submit;
-
 		vm.cancel = cancel;
+		vm.addTags = addTags;
+		vm.removeTag = removeTag;
 
-		if(vm.product && vm.product.productId){
-			vm.title = 'Edit: ' + vm.product.productName;
-		}else{
-			vm.title = "New Product";
+		activate();
+
+		function activate() {
+			if(vm.product && vm.product.productId){
+				vm.title = 'Edit: ' + vm.product.productName;
+			}else{
+				vm.title = "New Product";
+			}	
 		}
+		
 
 		function open($event) {
 			$event.preventDefault();
@@ -34,6 +38,20 @@
 
 		function cancel() {
 			$state.go('productList');
+		}
+
+		function addTags(tags) {
+			if(tags){
+				var array = tags.split(',');
+				vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+				vm.newTags = "";
+			}else{
+				alert("plase enter one or more tags seperated by commas");
+			}
+		}
+
+		function removeTag(idx) {
+			vm.product.tags.splice(idx,1);
 		}
 	}	
 })();
